1da6cb7797ed67b070529508528b0be3271cb7f0
mnaircckel <voiceofmarcel@gmail.com>
2015-06-17 20:49:04 +0000

    Working image uploads with dropbox

--- app/models/pollen_image.rb
+++ app/models/pollen_image.rb
@@ -1,8 +1,11 @@
 class PollenImage < ActiveRecord::Base
+  validates :title, :presence => true
   has_attached_file :image,
     :storage => :dropbox,
     :dropbox_credentials => {app_key: "g0nkzx98joscnb1", app_secret: "bsabyg3e2kr9087", 
                              access_token: "wta4ult7indaz4n6", access_token_secret: "z8rgt2npm3mg7zy",
-                             user_id: "49319691", access_type: "app_folder"}
+                             user_id: "49319691", access_type: "app_folder"},
+    :styles => { :medium => "800x800" , :thumb => "120x120>"},    
+    :dropbox_options => {:path => proc { |style| "#{style}/#{id}_#{image.original_filename}"}, :unique_filename => true}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
