cebe419e65d08c02cf2f4f4ae8c30d4093ddc063
mnaircckel <voiceofmarcel@gmail.com>
2015-06-23 20:40:25 +0000

    'Back' redirects to previous page in new, edit, and show to better work with searches

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # Funciton to overide normal devise functionality
+  def after_sign_in_path_for(resource)
+    pollen_images_path(resource) # <- After sign in, redirect to main index
+  end
 end
