cff183844da039e2b181926f69de300d399b9de0
mnaircckel <voiceofmarcel@gmail.com>
2015-06-23 22:40:23 +0000

    Changed Dimensions for better viewing

--- app/models/pollen_image.rb
+++ app/models/pollen_image.rb
@@ -6,7 +6,7 @@ class PollenImage < ActiveRecord::Base
     :dropbox_credentials => {app_key: "g0nkzx98joscnb1", app_secret: "bsabyg3e2kr9087", 
                              access_token: "wta4ult7indaz4n6", access_token_secret: "z8rgt2npm3mg7zy",
                              user_id: "49319691", access_type: "app_folder"},
-    :styles => { :medium => "800x800" , :thumb => "120x120>"},    
+    :styles => { :medium => "600x800" , :thumb => "120x120>"},    
     :dropbox_options => {:path => proc { |style| "#{style}/#{id}_#{image.original_filename}"}, :unique_filename => true}
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
