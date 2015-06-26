047253b2aaa5be34e3e09fe59edae3e39abf259a
mnaircckel <voiceofmarcel@gmail.com>
2015-06-26 20:13:38 +0000

    Added comments for simple search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,7 @@
 class SearchController < ApplicationController
   
+  # Search is simply done using form input which is sent to params
+  # See views/search/simple.html.erb for the search form
   def simple
   end
 
