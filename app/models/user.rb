9a55cb4d2ae96470d66a35e06a7a3f17c4491790
mnaircckel <voiceofmarcel@gmail.com>
2015-06-26 20:19:54 +0000

    Added comments to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Devise is used for admin authentacation. 
+  # You cannot manage the database without saying being logged in, 
+  # This is handled in the controllers with before_action :authenticate_user! 
+  # Check https://github.com/plataformatec/devise for more infomration.
   devise :database_authenticatable, :timeoutable
   
 end
