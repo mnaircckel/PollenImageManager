3cf918dd4a6bf638f338bfa70d6c6a271037778b
mnaircckel <voiceofmarcel@gmail.com>
2015-06-26 20:16:21 +0000

    Added comments to pollen_image.rb

--- app/models/pollen_image.rb
+++ app/models/pollen_image.rb
@@ -1,5 +1,8 @@
 class PollenImage < ActiveRecord::Base
   validates :title, :presence => true
+  # Attached files are handled with paperclip, see https://github.com/thoughtbot/paperclip for more information
+  # Attached files are sent to dropbox, this way storage can be accomplished on the cloud without using a premium database
+  # See https://github.com/janko-m/paperclip-dropbox for more information about paperclip and dropbox
   has_attached_file :image,
     :default_url => "/images/missing_image.png",
     :storage => :dropbox,
